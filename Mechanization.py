Fixed the longitude calculation bug

Mechanization.py
@@ -1,5 +1,6 @@
 from IntializationParameters import InitINS
 import numpy as np
+from decimal import Decimal
 
 
 class Mechanization (InitINS): 
@@ -16,11 +17,13 @@ class Mechanization (InitINS):
     def RadiiM (self):
         latitude = np.deg2rad(self._latitude)
         # print ("Latitude in radian", latitude )
-        self._Rm =  ( self.a *(1-self.e2) ) / ( (1- self.e2 * np.sin(latitude) * np.sin(latitude) )**(1.5) )
+        self._Rm =  ( self.a *(1-self.e2) ) / ( (1- self.e2 * np.sin(latitude) * np.sin(latitude) )**(1.5) )  
+        # self._Rm = 6378137.0  
         # print ("Meredian Radius", self._Rm )
     def RadiiN (self):
         latitude = np.deg2rad(self._latitude)
         self._Rn = self.a / np.sqrt ( 1- ( self.e2 * np.sin (latitude) * np.sin (latitude) ) )
+        # self._Rn = 6356752.3
         # print ("nORMAL Radius", self._Rn )
     ### Calculate Transformation from ECF to Local level frame
     def R_EL (self) :
@@ -209,9 +212,9 @@ class Mechanization (InitINS):
     
 
     def UpdateG (self):
-        # latitude = self._latitude
-        # Proj_Lat = np.sin(latitude) # Projection of the latitude after degree to raddian transformation
-        # self._Local_g = self.a1  * (1 + (self.a2* (Proj_Lat**2) ) + (   self.a3  * (Proj_Lat**4) ) ) + ( ( self.a4 + (self.a5 * (Proj_Lat**2)) ) *  self._altitude ) + self.a6 * (self._altitude**2)
+        latitude = np.deg2rad (self._latitude)
+        Proj_Lat = np.sin(latitude) # Projection of the latitude after degree to raddian transformation
+        self._Local_g = self.a1  * (1 + (self.a2* (Proj_Lat**2) ) + (   self.a3  * (Proj_Lat**4) ) ) + ( ( self.a4 + (self.a5 * (Proj_Lat**2)) ) *  self._altitude ) + self.a6 * (self._altitude**2)
         self._gVector = np.array([ 0, 0, - self._Local_g]) 
         # print ("updated g constant: ", self._gVector)
     def UpdateDeltaVelocity (self):
@@ -233,9 +236,10 @@ class Mechanization (InitINS):
     def UpdatePosition (self):
         ve_Prev, vn_Prev, vu_Prev = self._prev_vl[0], self._prev_vl[1], self._prev_vl[2]
         ve, vn, vu = self._vl[0], self._vl[1], self._vl[2]
-        self._altitude = self._altitude +  (0.5 * (vu + vu_Prev ) * self._delta_time)
-        self._latitude = self._latitude + np.rad2deg ( (0.5* (vn + vn_Prev ) * self._delta_time ) / (self._Rn + self._altitude) ) 
-        self._longitude = self._longitude + np.rad2deg ( ( 0.5 * (ve +  ve_Prev) * self._delta_time ) / ((self._Rn + self._altitude) * np.cos(self._latitude)) ) 
+        
+        self._longitude = self._longitude + np.rad2deg ( 0.5 * ( ve + ve_Prev) / ((self._Rn + self._altitude) * np.cos(np.deg2rad(self._latitude))) )  * self._delta_time
+        self._latitude = self._latitude + np.rad2deg ( 0.5 *  (vn + vn_Prev ) / (self._Rm + self._altitude) ) * self._delta_time
+        self._altitude = self._altitude +  ( 0.5 * (vu + vu_Prev)* self._delta_time ) 
         # print ("Updated Positions",  [self._latitude, self._longitude, self._altitude ])
          
     def CorrectAzimuth (self):
@@ -277,7 +281,7 @@ class Mechanization (InitINS):
         self.OMEGA_IE_L()
         self.OMEGA_EL_L()
         self.UpdateAccelerometers(fx,fy,fz)
-        self.UpdateG()
+        # self.UpdateG()
         self.UpdateDeltaVelocity ()
         self.UpdateVelocity ()
         ##### Stage 6: updating position components #######
@@ -324,7 +328,7 @@ class Mechanization (InitINS):
         self.OMEGA_IE_L()
         self.OMEGA_EL_L()
         self.UpdateAccelerometers(fx,fy,fz)
-        self.UpdateG()
+        # self.UpdateG()  
         self.UpdateDeltaVelocity ()
         self.UpdateVelocity ()
         ##### Stage 6: updating position components #######

Validate_mechanization_Kingston2008.py
@@ -14,19 +14,32 @@ Note: fy and wy from novatel should be negative , hence an operation of:
 import numpy as np
 import pandas as pd
 from Mechanization import Mechanization
-from loading_dataset import syncronize_INS, synchronize_general, load_IMU_dataset, load_REF_dataset
+from loading_dataset import syncronize_INS, synchronize_general, load_IMU_dataset, load_REF_dataset, load_IMU_data_no_downsampling
 import matplotlib.pyplot as plt
 import HelperFuncs
+import scipy.io as sio
 
 ####### files paths ########
 ref_file_path = r'C:\Users\marwa\Desktop\INS\INSfullMech\2008-06-23 kingston downtown\rover\Mat\INSPVA'
-imu_file_path = r'C:\Users\marwa\Desktop\INS\INSfullMech\2008-06-23 kingston downtown\rover\Mat\RAWIMU_denoised_LOD3_interpolated2'
+imu_or_file_path = r'C:\Users\marwa\Desktop\INS\INSfullMech\2008-06-23 kingston downtown\rover\Mat\RAWIMU_denoised_LOD3_interpolated2'
+imu_file_path = r'C:\Users\marwa\Desktop\INS\INSfullMech\f_w_IMSseconds_dt_Marwan.mat'
+
+sample_file_path = r'C:\Users\marwa\Desktop\INS\INSfullMech\sample_time.mat'
+
+sample_time_new = sio.loadmat(sample_file_path)
+sample_time_new_list = sample_time_new['sample_time'][0][:]
+
 Freq_INS = 10
 KVH = 0
 ref_data =  load_REF_dataset(ref_file_path, Freq_INS)
 # Reference Keys are:  dict_keys(['time', 'lat', 'lon', 'alt', 'roll', 'pitch', 'azimuth', 'Ve', 'Vn', 'Vu'])
-IMU_data = load_IMU_dataset(imu_file_path, KVH, Freq_INS)
-# IMU Keys are:  dict_keys(['time', 'fx', 'fy', 'fz', 'wx', 'wy', 'wz'])
+
+####### IMU data downsampled by me
+IMU_data = load_IMU_dataset(imu_or_file_path, KVH, Freq_INS)
+####### IMU data given by Dr. Malek
+# IMU_data = load_IMU_data_no_downsampling (imu_file_path,KVH)
+
+# IMU Keys are:  dict_keys(['time','sample_time' , 'fx', 'fy', 'fz', 'wx', 'wy', 'wz'])
 ref_data, IMU_data =  syncronize_INS(ref_data , IMU_data)
 
 print ("The Imu original first timestamp", IMU_data['time'][0])
@@ -39,6 +52,7 @@ wx_bias = -40.84e-007
 wy_bias = 12.3e-007 
 wz_bias = 2.06e-006
 ######## Remove Biases #########
+##### Note: If running Dr. Malek's given data please don't use biases
 IMU_data['fx'] -= fx_bias
 IMU_data['fy'] -= fy_bias
 IMU_data['fz'] -= fz_bias
@@ -48,24 +62,27 @@ IMU_data['wz'] -= wz_bias
 ############# Mechanization Intialization Params ###########
 start_time = 600 # seconds
 start = start_time * Freq_INS
+Initial = start - 1
 Init_lat, Init_lon, Init_alt = ref_data ['lat'][start], ref_data ['lon'][start], ref_data ['alt'][start]
 Init_roll, Init_pitch, Init_azimuth = ref_data ['roll'][start], ref_data ['pitch'][start], ref_data ['azimuth'][start]
 Init_ve, Init_vn, Init_vu = ref_data ['Ve'][start], ref_data ['Vn'][start], ref_data ['Vu'][start]
-dt = 0.1 # 10 Hz
+dt = sample_time_new_list [start]#IMU_data['time'][1] - IMU_data['time'][0] # 10 Hz
 ############ Initialize Mecanization ##############
 ################# consruct an instance of the mechanization
 INS_Mechanize = Mechanization (Init_lat, Init_lon, Init_alt, Init_roll, Init_pitch, Init_azimuth, dt)
 INS_Mechanize.Init_Velocity(Init_ve, Init_vn, Init_vu)
 duration = len(IMU_data['fx'])
+# INS_Mechanize._Local_g = 9.805359489865625
 
-Lat, Lon, Alt = [],[], []
-Roll, Pitch, Azimuth = [],[],[]
-ve,vn,vu = [],[],[]
+Lat, Lon, Alt = [Init_lat],[Init_lon], [Init_alt]
+Roll, Pitch, Azimuth = [Init_roll],[Init_pitch],[Init_azimuth]
+ve,vn,vu = [Init_ve],[Init_vn],[Init_vu]
 
 ################# Loop over the mechanization ##########
 for i in range (start,duration): 
     wx, wy, wz = IMU_data['wx'][i], IMU_data['wy'][i], IMU_data['wz'][i]
     fx, fy, fz = IMU_data['fx'][i], IMU_data['fy'][i], IMU_data['fz'][i]
+    INS_Mechanize.delta_time = IMU_data['time'][i] - IMU_data['time'][i-1] # sample_time_new_list[i] #
     INS_Mechanize.compile_standalone(wx, wy, wz,fx, fy, fz)
     Lat.append(INS_Mechanize.latitude)
     Lon.append(INS_Mechanize.longitude)
@@ -81,16 +98,16 @@ for i in range (start,duration):
 ######## Prepare INS standalone and ref Output ######
 imu_data_mechanized = dict()
 ref_data_trimmed = dict()
-imu_data_mechanized['time'], ref_data_trimmed['time'] = IMU_data['time'][start:duration], ref_data['time'][start:duration]
-imu_data_mechanized['lat'], ref_data_trimmed['lat'] = Lat, ref_data['lat'][start:duration]
-imu_data_mechanized['lon'], ref_data_trimmed['lon']  = Lon, ref_data['lon'][start:duration]
-imu_data_mechanized['alt'], ref_data_trimmed['alt'] = Alt, ref_data['alt'][start:duration]
-imu_data_mechanized['roll'], ref_data_trimmed['roll'] = Roll, ref_data['roll'][start:duration]
-imu_data_mechanized['pitch'], ref_data_trimmed['pitch'] = Pitch, ref_data['pitch'][start:duration]
-imu_data_mechanized['azimuth'], ref_data_trimmed['azimuth'] = Azimuth, ref_data['azimuth'][start:duration]
-imu_data_mechanized['ve'], ref_data_trimmed['ve'] = ve, ref_data['Ve'][start:duration]
-imu_data_mechanized['vn'], ref_data_trimmed['vn'] = vn, ref_data['Vn'][start:duration]
-imu_data_mechanized['vu'], ref_data_trimmed['vu'] = vu, ref_data['Vu'][start:duration]
+imu_data_mechanized['time'], ref_data_trimmed['time'] = IMU_data['time'][Initial:duration], ref_data['time'][Initial:duration]
+imu_data_mechanized['lat'], ref_data_trimmed['lat'] = Lat, ref_data['lat'][Initial:duration]
+imu_data_mechanized['lon'], ref_data_trimmed['lon']  = Lon, ref_data['lon'][Initial:duration]
+imu_data_mechanized['alt'], ref_data_trimmed['alt'] = Alt, ref_data['alt'][Initial:duration]
+imu_data_mechanized['roll'], ref_data_trimmed['roll'] = Roll, ref_data['roll'][Initial:duration]
+imu_data_mechanized['pitch'], ref_data_trimmed['pitch'] = Pitch, ref_data['pitch'][Initial:duration]
+imu_data_mechanized['azimuth'], ref_data_trimmed['azimuth'] = Azimuth, ref_data['azimuth'][Initial:duration]
+imu_data_mechanized['ve'], ref_data_trimmed['ve'] = ve, ref_data['Ve'][Initial:duration]
+imu_data_mechanized['vn'], ref_data_trimmed['vn'] = vn, ref_data['Vn'][Initial:duration]
+imu_data_mechanized['vu'], ref_data_trimmed['vu'] = vu, ref_data['Vu'][Initial:duration]
 
 ##############Plotting ###################
 ############## Full Diagnosis ################

loading_dataset.py
@@ -15,6 +15,17 @@ def down_sample(signal , down_sample_bin ):
         signal_new[i] = np.mean(signal[frm:to])
     return signal_new
 
+def down_sample_time(signal , down_sample_bin ):
+    L = len(signal)
+    L_new = int(np.floor(L/down_sample_bin))
+    signal_new = np.zeros(L_new)
+
+    for i in range(L_new):
+        pick_time = i * down_sample_bin
+        signal_new[i] = signal[pick_time]
+    return signal_new
+
+
 def upsample_signal(signal , up_sample_factor):
     d = 1.0/up_sample_factor
     return np.interp(np.arange(0, len(signal), d), np.arange(0, len(signal)), signal)
@@ -37,7 +48,7 @@ def load_IMU_dataset(imu_file_path, IMU_CHOICE, Freq_INS):
     down_sampleBin_imu = int(np.round(Freq_imu / Freq_INS))
     print('downsampling window  = ' , down_sampleBin_imu)
     #downsample time
-    time_imu = down_sample(time_imu , down_sampleBin_imu)
+    time_imu = down_sample_time(time_imu , down_sampleBin_imu)
     #-----------------------------------------------
     #accelorometer data
     fx_imu = imu_data['f']['x'][0,0][:,0]
@@ -81,6 +92,44 @@ def load_IMU_dataset(imu_file_path, IMU_CHOICE, Freq_INS):
     imu_data['wy'] = wy_imu
     imu_data['wz'] = wz_imu   
     
+    return imu_data
+
+def load_IMU_data_no_downsampling (imu_file_path, IMU_CHOICE ):
+    imu_data = sio.loadmat(imu_file_path)
+    #get all important data
+    #Time
+    time_imu = imu_data.get('IMU_second')[0][:] 
+    #sample time
+    dt_imu = imu_data.get('sample_time')[0][:]
+    
+    #get imu frequency
+    Freq_imu = np.round(1 / np.mean(time_imu[1:] - time_imu[:-1]))
+    print('imu frequency = ' , Freq_imu , ' Hz')
+    #-----------------------------------------------
+    #accelorometer data
+    fx_imu = imu_data['f']['x'][0,0][0,:]
+    fy_imu = imu_data['f']['y'][0,0][0,:]
+    # if IMU_CHOICE == KVH:
+    #     fy_imu = fy_imu* -1 # special case for the accelerometer of imu is inverted
+    fz_imu = imu_data['f']['z'][0,0][0,:]
+    #-----------------------------------------------
+    #Gyro data
+    wx_imu = imu_data['w']['x'][0,0][0,:]
+    wy_imu = imu_data['w']['y'][0,0][0,:] 
+    # if IMU_CHOICE == KVH:
+    #     wy_imu = wy_imu* -1 # special case for the accelerometer of imu is inverted
+    wz_imu = imu_data['w']['z'][0,0][0,:]
+
+    imu_data = dict()
+    imu_data['time'] = time_imu
+    imu_data['sample_time'] = dt_imu
+    imu_data['fx'] = fx_imu
+    imu_data['fy'] = fy_imu
+    imu_data['fz'] = fz_imu
+    imu_data['wx'] = wx_imu
+    imu_data['wy'] = wy_imu
+    imu_data['wz'] = wz_imu   
+
     return imu_data
 #################################################################################
 ######################### Loading Ref Data ######################################